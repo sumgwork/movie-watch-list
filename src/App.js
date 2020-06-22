d80299cb9c8d71b2346ea5c0edeb9b69a4d2eefb
Sumit Govil <sumit.govil@optus.com.au>
2020-06-22 11:20:42 +1000

    scss and header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import './App.css';
+import './App.scss';
 import store from './redux/store';
+import Header from './components/header/Header';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div className="App">Redux</div>
+      <Header />
+      <div className="app">
+        <h1>Setup React Redux</h1>
+      </div>
     </Provider>
   );
 };
