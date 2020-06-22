3eab09ff668aeec823355211ea464ba0a514b3a9
Sumit Govil <sumit.govil@optus.com.au>
2020-06-22 12:58:32 +1000

    Grid component

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Slideshow from '../slide-show/Slideshow';
 import './MainContent.scss';
 import Paginate from '../paginate/Paginate';
+import Grid from '../grid/Grid';
 
 const MainContent = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -17,12 +18,75 @@ const MainContent = () => {
   const images = [
     {
       url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 2.5,
+    },
+    {
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5,
     },
     {
       url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 7.8,
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 9.7,
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 6.5,
     },
     {
       url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5,
+    },
+    {
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 5.5,
+    },
+    {
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5,
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 7.8,
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 9.7,
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 6.5,
+    },
+    {
+      url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5,
+    },
+    {
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 5.5,
+    },
+    {
+      url: 'https://images.pexels.com/photos/688574/pexels-photo-688574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5,
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 7.8,
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 9.7,
+    },
+    {
+      url: 'https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 6.5,
+    },
+    {
+      url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 8.5,
     },
   ];
   return (
@@ -34,7 +98,7 @@ const MainContent = () => {
           <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
         </div>
       </div>
-      {/* display grid component */}
+      <Grid images={images} />
     </div>
   );
 };
